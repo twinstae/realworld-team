eb0317254cbf44ac32d16cc9697f9a6b9328903b
twinstae <rabolution@gmail.com>
2021-02-01 10:38:09 +0900

    index.html, js
    src= ./dist/bundle.js

---
+++ front/src/components/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../css/styles.css'
+
+export default function App(){
+    
+    console.log("i am in app");
+    return(<div>Hello World</div>)
+}
---
+++ front/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App'
+
+console.log("i am in index");
+ReactDOM.render(<App />, document.getElementById('app'))
