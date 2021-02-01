84b329bc104f3b1c536719d09a4de722fb98021d
Yummy-sk <60822846+Yummy-sk@users.noreply.github.com>
2021-02-01 21:54:30 +0900

    first-commit
    realworld project start

--- front/src/components/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../css/styles.css'
-
-export default function App(){
-    
-    console.log("i am in app");
-    return(<div>Hello World</div>)
-}
--- front/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App'
-
-console.log("i am in index");
-ReactDOM.render(<App />, document.getElementById('app'))
