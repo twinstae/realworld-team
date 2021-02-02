5bce7baf2dd2a06b6747eea595ae8a9d58ad316a
YSH <sheepsh93@gmail.com>
2021-02-02 15:38:44 +0900

    UserRepository, UserServiceImpl 일부 수정

--- back/realWorld/src/main/java/study/realWorld/repository/UserRepository.java
+++ back/realWorld/src/main/java/study/realWorld/repository/UserRepository.java
@@ -3,8 +3,6 @@ package study.realWorld.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.realWorld.entity.User;
 
-import java.util.List;
-
 public interface UserRepository extends JpaRepository<User, Long> {
     User findOneByEmail(String email);
 }
