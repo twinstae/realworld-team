7752f432ef7a9c63e660582b9b4286733cb18066
twinstae <rabolution@gmail.com>
2021-02-09 13:39:23 +0900

    UserRepository findUserWithAuthorities Test

--- back/realWorld/src/main/java/study/realWorld/entity/Authority.java
+++ back/realWorld/src/main/java/study/realWorld/entity/Authority.java
@@ -1,5 +1,6 @@
 package study.realWorld.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,7 @@ import javax.persistence.Id;
 @Entity
 @Getter
 @Builder
+@AllArgsConstructor
 @NoArgsConstructor
 public class Authority {
 
--- back/realWorld/src/test/java/study/realWorld/repository/UserRepositoryTest.java
+++ back/realWorld/src/test/java/study/realWorld/repository/UserRepositoryTest.java
@@ -1,35 +1,44 @@
 package study.realWorld.repository;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import study.realWorld.api.exception.ResourceNotFoundException;
 import study.realWorld.entity.Authority;
 import study.realWorld.entity.User;
 
 import java.util.Collections;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringBootTest
 class UserRepositoryTest {
 
+    @Autowired
+    AuthorityRepository authorityRepository;
 
     @Autowired
     UserRepository userRepository;
 
-    @Test
-    public void init() throws Exception {
+    String email = "user1@gmail.com";
 
+    @BeforeEach
+    public void init() throws Exception {
         Authority authority = Authority.builder()
                 .authorityName("ROLE_USER")
                 .build();
 
+        authorityRepository.save(authority);
 
         User user = User
                 .builder()
                 .userName("user1")
-                .email("user1@gmail.com")
+                .email(email)
                 .password("123")
                 .activated(true)
                 .authorities((Collections.singleton(authority)))
@@ -38,15 +47,17 @@ class UserRepositoryTest {
         userRepository.save(user);
     }
 
-    @Test
-    public void findtest() throws Exception {
-        // given
-
-        // when
-
-        // then
-
+    @AfterEach
+    public void tearDown() throws Exception {
+        userRepository.deleteAll();
     }
 
+    @DisplayName("Email을 가지고 User를 Authorities와 join해서 가져온다.")
+    @Test
+    public void findUserWithAuthoritiesTest() throws Exception {
+        User user = userRepository.findOneWithAuthoritiesByEmail(email).orElseThrow(ResourceNotFoundException::new);
 
+        assertThat(user.getEmail()).isEqualTo(email);
+        assertThat(user.getAuthorities().isEmpty()).isFalse();
+    }
 }
