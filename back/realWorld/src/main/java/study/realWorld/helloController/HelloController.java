2a2f321151d9b556a68e74e5179709289ff21b3d
YSH <sheepsh93@gmail.com>
2021-02-09 12:08:26 +0900

    테스트용 helloController 수정

--- back/realWorld/src/main/java/study/realWorld/helloController/HelloController.java
+++ back/realWorld/src/main/java/study/realWorld/helloController/HelloController.java
@@ -1,15 +1,16 @@
 package study.realWorld.helloController;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class HelloController {
 
-
     @GetMapping("/hello")
-    public String hello() {
-
-        return "hello";
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("hello");
     }
 }
