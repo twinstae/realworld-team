ff02424625be0335536407136676eff0dc12cc02
twinstae <rabolution@gmail.com>
2021-02-09 14:33:26 +0900

    Test TokenProvider's createToken, getAuthorities, validateToken

--- back/realWorld/src/test/java/study/realWorld/jwts/TokenProviderTest.java
+++ back/realWorld/src/test/java/study/realWorld/jwts/TokenProviderTest.java
@@ -1,36 +1,25 @@
 package study.realWorld.jwts;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.TestingAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import study.realWorld.api.dto.ArticleCreateDto;
-import study.realWorld.api.dto.ArticleDto;
-import study.realWorld.api.exception.ResourceNotFoundException;
-import study.realWorld.entity.Articles;
 import study.realWorld.jwt.TokenProvider;
-import study.realWorld.repository.ArticlesRepository;
+
+import java.security.Principal;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 
 @SpringBootTest()
 public class TokenProviderTest {
 
     TokenProvider tokenProvider;
 
-    @Autowired
-    AuthenticationManagerBuilder authenticationManagerBuilder;
-
     @BeforeEach
     public void createTokenProvider () throws Exception {
-        String secret = "s1e2c3r4e5t6s1e2c3r4e5t6s1e2c3r4e5t6s1e2c3r4e5t6s1e2c3r4e5t6s1e2c3r4e5t6";
+        String base = "s1e2c3r4e5t6s1e2c3r4e5t6s1e2c3r4e5t6s1e2c3r4e5t6s1e2c3r4e5t6s1e2c3r4e5t6";
+        String secret = base+base;
         long tokenValidityInSeconds = 60L * 60L * 24L;
         tokenProvider = new TokenProvider(secret, tokenValidityInSeconds);
         tokenProvider.afterPropertiesSet();
@@ -40,31 +29,27 @@ public class TokenProviderTest {
 
     @Test
     public void createTokenTest () throws Exception {
-
-    }
-
-    @Test
-    public void getAuthenticationTest() throws Exception {
-
-    }
-
-    @Test
-    public void JWTBuilderTest() throws Exception {
-
         String username = "user1";
-        String password = "password";
 
-        UsernamePasswordAuthenticationToken authenticationToken= new UsernamePasswordAuthenticationToken(username,password);
-        System.out.println("authenticationToken = " + authenticationToken);
+        Principal principal = new Principal() {
+            @Override
+            public String getName() {
+                return username;
+            }
+        };
+        String[] authorities = {"USER"};
 
-        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        Authentication authentication = new TestingAuthenticationToken(principal, null, authorities);
         System.out.println("authentication = " + authentication);
-
 
         String jwt = tokenProvider.createToken(authentication);
         System.out.println("jwt = " + jwt);
 
+        Authentication auth_token = tokenProvider.getAuthentication(jwt);
+        System.out.println(auth_token);
+
+        assertThat(tokenProvider.validateToken(jwt)).isTrue();
+
+        assertThat("nothing").isEqualTo("error!");
     }
-
-
 }
