1e00b53682624600bdb235c99a1fd7e6573cff25
YSH <sheepsh93@gmail.com>
2021-02-09 12:14:05 +0900

    다음의 코드에서 질문 발생

--- back/realWorld/src/main/java/study/realWorld/config/SecurityConfig.java
+++ back/realWorld/src/main/java/study/realWorld/config/SecurityConfig.java
@@ -4,15 +4,15 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
-
-    @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception {
-
-        httpSecurity
-                .authorizeRequests()
-                .antMatchers("/api/hello").permitAll()
-                .anyRequest().authenticated();
-    }
-}
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter{
+//
+//    @Override
+//    protected void configure(HttpSecurity httpSecurity) throws Exception {
+//
+////        httpSecurity
+////                .authorizeRequests()
+////                .antMatchers("/api/hello").permitAll()
+////                .anyRequest().authenticated();
+//    }
+//}
