e55643a27bfee0b2e49ad8b68648e9cee00996b7
YSH <sheepsh93@gmail.com>
2021-02-09 19:17:56 +0900

    validateToken 테스트 추가. (성공하는 케이스만 추가했고, Exception 케이스는 못했음.)

--- back/realWorld/src/test/java/study/realWorld/jwts/TokenProviderTest.java
+++ back/realWorld/src/test/java/study/realWorld/jwts/TokenProviderTest.java
@@ -1,13 +1,21 @@
 package study.realWorld.jwts;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.authentication.TestingAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import study.realWorld.jwt.TokenProvider;
 
+import java.security.Key;
 import java.security.Principal;
+import java.util.Date;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -16,11 +24,16 @@ public class TokenProviderTest {
 
     TokenProvider tokenProvider;
 
+    String secret;
+    String AUTHORITIES_KEY = "auth";
+    Date validity;
+
     @BeforeEach
     public void createTokenProvider () throws Exception {
         String base = "s1e2c3r4e5t6s1e2c3r4e5t6s1e2c3r4e5t6s1e2c3r4e5t6s1e2c3r4e5t6s1e2c3r4e5t6";
-        String secret = base+base;
+        secret = base+base;
         long tokenValidityInSeconds = 60L * 60L * 24L;
+        validity = new Date( (new Date()).getTime() + tokenValidityInSeconds);
         tokenProvider = new TokenProvider(secret, tokenValidityInSeconds);
         tokenProvider.afterPropertiesSet();
         System.out.println(tokenProvider);
@@ -50,6 +63,21 @@ public class TokenProviderTest {
 
         assertThat(tokenProvider.validateToken(jwt)).isTrue();
 
-        assertThat("nothing").isEqualTo("error!");
+
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        Key key = Keys.hmacShaKeyFor(keyBytes);
+
+
+        String token = Jwts.builder()
+                .setSubject(authentication.getName())
+                .claim(AUTHORITIES_KEY, authorities)
+                .signWith(key, SignatureAlgorithm.HS512)
+                .setExpiration(validity)
+                .compact();
+
+        System.out.println("token parsing = " + Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token));
+
+        assertThat(Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody().containsValue("USER"));
+        //assertThat("nothing").isEqualTo("error!");
     }
 }
