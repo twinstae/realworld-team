72c6fe90ad6d14596af9eec5a9cf3af7e6fea577
twinstae <rabolution@gmail.com>
2021-02-15 16:48:00 +0900

    AuthortiyfindByAuthorityName 메서드 추가

--- back/realWorld/src/main/java/study/realWorld/repository/AuthorityRepository.java
+++ back/realWorld/src/main/java/study/realWorld/repository/AuthorityRepository.java
@@ -3,5 +3,8 @@ package study.realWorld.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.realWorld.entity.Authority;
 
+import java.util.Optional;
+
 public interface AuthorityRepository extends JpaRepository<Authority, String> {
+    Optional<Authority> findByAuthorityName(String authorityName);
 }
