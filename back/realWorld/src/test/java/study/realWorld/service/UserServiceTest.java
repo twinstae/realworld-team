cc3cbd3d3a6483241c5ab49d65792fca488f8f58
twinstae <rabolution@gmail.com>
2021-02-17 17:25:59 +0900

    Update UserServiceTest.java

--- back/realWorld/src/test/java/study/realWorld/service/UserServiceTest.java
+++ back/realWorld/src/test/java/study/realWorld/service/UserServiceTest.java
@@ -17,7 +17,7 @@ public class UserServiceTest extends TestingUtil {
     @DisplayName("userSipnUpDto로 가입하면,repository에 같은 email을 가진 user가 존재한다")
     @Test
     public void userSignUpTest(){
-        authorityInitRoleUser();
+        authorityInit();
 
         userService.signUp(userSignUpDto);
         Optional<User> user = userRepository.findOneWithAuthoritiesByEmail(userSignUpDto.getEmail());
@@ -27,7 +27,7 @@ public class UserServiceTest extends TestingUtil {
     @DisplayName("이미 가입한 이메일로 다시 가입하려 하면, RuntimeException을 던진다")
     @Test
     public void userSignUpWithAlreadyExistEmailTest(){
-        authorityInitRoleUser();
+        authorityInit();
 
         userService.signUp(userSignUpDto); // 이미 가입
 
