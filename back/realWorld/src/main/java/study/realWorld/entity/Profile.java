6264cef5cf5a6b94db46ae996638a1cbf45ff336
YSH <sheepsh93@gmail.com>
2021-02-19 16:22:43 +0900

    follow 기능을 위한 Profile Entity 수정.(관계 수정) => 기존에 생성했던 Follow Entity는 삭제했음.

--- back/realWorld/src/main/java/study/realWorld/entity/Profile.java
+++ back/realWorld/src/main/java/study/realWorld/entity/Profile.java
@@ -1,10 +1,18 @@
 package study.realWorld.entity;
 
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 @Entity
 public class Profile {
 
@@ -21,11 +29,29 @@ public class Profile {
     @JoinColumn(name ="USER_ID")
     private User user;
 
-    @OneToMany(mappedBy = "from_user")
-	private final List<Follow> from_user = new ArrayList<>();
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(schema = "TEST", name = "USER_RELATIONS",
+            joinColumns = @JoinColumn(name = "FOLLOWED_ID"),
+            inverseJoinColumns = @JoinColumn(name = "FOLLOWER_ID"))
+    private Set<Profile> followers =  new HashSet<Profile>();;
 
-	@OneToMany(mappedBy = "to_user")
-	private final List<Follow> to_user = new ArrayList<>();
 
+
+    @Builder
+    public Profile(String username, String image, User user, Set<Profile> followers) {
+        this.username = username;
+        this.image = image;
+        this.user = user;
+        this.followers =followers;
+    }
+
+    public boolean checkFollow(Profile profile) {
+        return this.followers.contains(profile);
+    }
+
+
+    private void addFollower(Profile follower) {
+        followers.add(follower);
+    }
 
 }
