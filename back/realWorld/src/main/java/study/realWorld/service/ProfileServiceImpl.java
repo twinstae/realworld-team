7b1eec9e9807cc0d3c6e37c433df9a0c6013fe3a
YSH <sheepsh93@gmail.com>
2021-02-19 16:26:45 +0900

    ProfileService findByUsername생성

--- back/realWorld/src/main/java/study/realWorld/service/ProfileServiceImpl.java
+++ back/realWorld/src/main/java/study/realWorld/service/ProfileServiceImpl.java
@@ -2,8 +2,15 @@ package study.realWorld.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import study.realWorld.api.dto.articleDtos.ArticleCreateDto;
+import study.realWorld.api.dto.articleDtos.ArticleDto;
+import study.realWorld.api.dto.profilesDtos.ProfileCreateDto;
 import study.realWorld.api.dto.profilesDtos.ProfileDto;
 import study.realWorld.api.exception.ResourceNotFoundException;
+import study.realWorld.entity.Articles;
+import study.realWorld.entity.Profile;
+import study.realWorld.entity.User;
 import study.realWorld.repository.ProfilesRepository;
 
 @RequiredArgsConstructor
@@ -11,10 +18,29 @@ import study.realWorld.repository.ProfilesRepository;
 public class ProfileServiceImpl implements ProfilesService{
 
     private final ProfilesRepository profilesRepository;
+    private final UserService userService;
 
     @Override
+    @Transactional(readOnly = true)
     public ProfileDto findByUsername(String username) {
-       return profilesRepository.findProfilesByUsername(username)
-               .orElseThrow(ResourceNotFoundException::new);
+        User currentUser =  userService.getMyUser(); //현재 접속한 user
+        Profile currentUserProfile =profilesRepository.findProfilesByUsername(currentUser.getUserName())
+                .orElseThrow(ResourceNotFoundException::new);
+
+        Profile targetUserProfile = profilesRepository.findProfilesByUsername(username).orElseThrow(RuntimeException::new);
+        boolean isFollowed = targetUserProfile.checkFollow(currentUserProfile);
+        return ProfileDto.fromEntity(targetUserProfile,isFollowed);
     }
+
+
+//    @Transactional
+//    @Override
+//    public ProfileDto save(ProfileCreateDto profileCreateDto) {
+//        User currentUser =  userService.getMyUser(); //현재 접속한 user
+//        Profile findProfile =profilesRepository.findProfilesByUsername(currentUser.getUserName())
+//                .orElseThrow(ResourceNotFoundException::new);
+//        Profile profile = profilesRepository.save(ProfileCreateDto.toEntity(currentUser));
+//        return ProfileDto.fromEntity(profile);
+//    }
+
 }
--- back/realWorld/src/main/java/study/realWorld/service/ProfilesService.java
+++ back/realWorld/src/main/java/study/realWorld/service/ProfilesService.java
@@ -1,9 +1,13 @@
 package study.realWorld.service;
 
+import study.realWorld.api.dto.articleDtos.ArticleCreateDto;
 import study.realWorld.api.dto.articleDtos.ArticleDto;
+import study.realWorld.api.dto.profilesDtos.ProfileCreateDto;
 import study.realWorld.api.dto.profilesDtos.ProfileDto;
 
 public interface ProfilesService {
 
     ProfileDto findByUsername(String username);
+
+   // ProfileDto save(ProfileCreateDto profileCreateDto);
 }
