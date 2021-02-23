fa78021729347c096db02b755693550b206e4d1c
twinstae <rabolution@gmail.com>
2021-02-23 16:06:22 +0900

    ProfileEnitityTest 리팩토링, beforeEach로 추출

--- back/realWorld/src/test/java/study/realWorld/entity/ProfileEntityTest.java
+++ back/realWorld/src/test/java/study/realWorld/entity/ProfileEntityTest.java
@@ -1,5 +1,6 @@
 package study.realWorld.entity;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import study.realWorld.TestingUtil;
@@ -9,18 +10,24 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class ProfileEntityTest extends TestingUtil {
 
+    Profile profile1;
+    Profile profile2;
+
+    @BeforeEach
+    public void setUp(){
+        createUserInit();
+        anotherUserInit();
+
+        profile1 = profileRepository.findOneByUsername(userSignUpDto.getUsername())
+                .orElseThrow(RuntimeException::new);
+        profile2 = profileRepository.findOneByUsername(userSignUpDto2.getUsername())
+                .orElseThrow(RuntimeException::new);
+    }
+
     @Transactional
     @DisplayName("한 profile이 다른 프로파일을 follow하면 isFollow는 true이다")
     @Test
     public void followTest(){
-        createUserInit();
-        anotherUserInit();
-
-        Profile profile1 = profileRepository.findOneByUsername(userSignUpDto.getUsername())
-                .orElseThrow(RuntimeException::new);
-        Profile profile2 = profileRepository.findOneByUsername(userSignUpDto2.getUsername())
-                .orElseThrow(RuntimeException::new);
-
         profile1.follow(profile2);
 
         assertThat(profile1.isFollow(profile2)).isTrue();
@@ -31,17 +38,10 @@ public class ProfileEntityTest extends TestingUtil {
     @DisplayName("한 Profile이 다른 Profile을 unFollow하면 isFollow는 false이다")
     @Test
     public void unFollowTest() throws Exception {
-        createUserInit();
-        anotherUserInit();
+        followTest();
 
-        Profile profile1 = profileRepository.findOneByUsername(userSignUpDto.getUsername())
-                .orElseThrow(RuntimeException::new);
-        Profile profile2 = profileRepository.findOneByUsername(userSignUpDto2.getUsername())
-                .orElseThrow(RuntimeException::new);
-
-        profile1.follow(profile2);
-        assertThat(profile1.isFollow(profile2)).isTrue();
         profile1.unfollow(profile2);
+
         assertThat(profile1.isFollow(profile2)).isFalse();
     }
 
