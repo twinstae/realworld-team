3dcf10d35090ed1cd6113b7aef45d51bea80edbd
twinstae <rabolution@gmail.com>
2021-04-29 00:29:53 +0900

    [react] 스토리북 테스트

--- front/src/components/Article.tsx
+++ front/src/components/Article.tsx
@@ -1,17 +1,28 @@
 import {FunctionComponent} from "react";
 
-type ArticleProp = {
+export type ArticleProp = {
   article: {
-    body: string
+    createdAt: string,
+    updatedAt: string,
+    id: number,
+    slug: string,
+    title: string,
+    description: string,
+    body: string,
+    tagList: string[],
+    //author: Profile
+    favorited: boolean,
+    favoritesCount: number,
   }
 }
 
 export const Article: FunctionComponent<ArticleProp> = ({article}) =>
   (
-    <div>
-      {/* 헤더 */}
-      {/* ??? */}
-      {article.body}
-      {/* ??? */}
+    <div className="Article">
+      <div className="ArticleHeader">{article.createdAt}</div>
+      <div className="ArticlePreview">
+        <h3>{article.title}</h3>
+        <p>{article.description}</p>
+      </div>
     </div>
-  );
+);
